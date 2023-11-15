6adb0009b6fa0c76ecdfe4e603431b25bfd6ea6f
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2023-11-15 12:41:36 +0000

    Project

--- Project/project-ltw16g03/templates/footer.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php function drawFooter() { ?>
-    </main>
-        <footer>
-        <p>&copy; 2023 HelpDesk. Project made by Rafael Alves, José Guedes and Manuel Alves.</p>
-        </footer>
-    </body>
-<?php } ?>
